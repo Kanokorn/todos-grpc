f71247e720c391f94d1ef1afe7ff04cd5b314a41
Kanokorn Chongnguluam <kanokorn.may@gmail.com>
2021-08-15 19:33:59 +0700

    Graceful shutdown

--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
 	"todos/internal/server"
 	"todos/internal/storage"
 	"todos/proto"
@@ -12,10 +16,16 @@ import (
 )
 
 func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
 	port := os.Getenv("TODO_GRPC_PORT")
 	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return fmt.Errorf("failed to listen: %w", err)
 	}
 
 	var opts []grpc.ServerOption
@@ -23,5 +33,18 @@ func main() {
 	inMemory := storage.NewInMemory()
 	proto.RegisterTodoServiceServer(grpcServer, server.NewServer(inMemory))
 
-	grpcServer.Serve(lis)
+	go func() {
+		ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+		<-ctx.Done()
+		grpcServer.GracefulStop()
+		log.Println("shutting down server...")
+	}()
+
+	log.Printf("server started at port: %s\n", port)
+	if err := grpcServer.Serve(lis); err != nil {
+		return fmt.Errorf("failed to serve: %w", err)
+	}
+
+	log.Println("server exited properly")
+	return nil
 }
